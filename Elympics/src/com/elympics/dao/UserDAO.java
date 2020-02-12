f35e7552e135ff37f9bb00fc438531583c8afbb8
alberto <alberto@mail.com>
2020-02-12 16:01:27 +0100

    Commit commento

--- Elympics/src/com/elympics/dao/UserDAO.java
+++ Elympics/src/com/elympics/dao/UserDAO.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import com.elympics.bean.User;
 
 public interface UserDAO {
+	/* Utente */
 	public boolean CheckUser(User user) throws Exception ;
 	public void AddUser(User user) throws Exception ;
 	public void DeleteUser(User user) throws Exception ;
