075a58f3c7cf59c823b6558a5d66d49e233ab18b
FadaAstir <59929974+FadaAstir@users.noreply.github.com>
2020-04-15 10:28:24 +0200

    commit controller pre pull

--- Elympics/src/com/elympics/controller/UserController.java
+++ Elympics/src/com/elympics/controller/UserController.java
@@ -10,31 +10,5 @@ import com.elympics.manager.UserManager;
 @Controller
 public class UserController {
 
-	@RequestMapping("/login")
-	//read the provided form data
-	public String display(@RequestParam("username") String username,@RequestParam("password") String password,Model m)
-	{
-		UserManager manager = new UserManager();
-		User user = null;
-		try {
-			user = manager.login(username, password);
-		} catch (Exception e) {
-			e.printStackTrace();
-			m.addAttribute("message", e.getMessage());
-			return "errorpage";
-		}
-		if(user!=null)
-		{
-			String msg="Hello "+ user.getNome() + " " + user.getCognome();
-			//add a message to the model
-			m.addAttribute("message", msg);
-			return "viewpage";
-		}
-		else
-		{
-			String msg="Sorry "+ username +". You entered an incorrect password or username";
-			m.addAttribute("message", msg);
-			return "errorpage";
-		}	
-	}
+
 }
